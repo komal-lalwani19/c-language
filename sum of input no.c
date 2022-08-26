c3ee54b4db17891e00de8f3245248a3c556173ec
komal lalwani <111520905+komal-lalwani19@users.noreply.github.com>
2022-08-26 13:42:48 +0530

    Create sum of input no.c

---
+++ sum of input no.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int no,b,sum=0;
+	printf("enter any number");
+	scanf("%d",&no);
+	while(no!=0)
+	{
+		b=no%10;
+		sum=sum+b;
+		no=no/10;
+	}
+	printf("the sum of digit is %d",sum);
+}
