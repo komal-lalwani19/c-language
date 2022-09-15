6ecb02569264f6420857570051cdd1345f962e2e
komal lalwani <111520905+komal-lalwani19@users.noreply.github.com>
2022-09-16 00:39:08 +0530

    Add files via upload

---
+++ compare string.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char name[200]="komal";
+	char lname[200]="komal";
+	if(strcmp(name,lname)==0)
+	printf("string equal");
+	else
+	printf("not equal");
+}
+
